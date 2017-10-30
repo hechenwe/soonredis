7a5431c45c0271adec617a121d34c62ed0544aaf
hechen <hechen@e-eduspace.com>
2017-10-30 09:59:50 +0800

    更新

--- soonredis/soonredis/src/main/java/com/sooncode/soonredis/service/SoonRedisDao.java
+++ soonredis/soonredis/src/main/java/com/sooncode/soonredis/service/SoonRedisDao.java
@@ -1,5 +1,8 @@
 package com.sooncode.soonredis.service;
 
+import java.util.Map;
+import java.util.Map.Entry;
+
 import redis.clients.jedis.Jedis;
 
 public class SoonRedisDao {
@@ -8,13 +11,10 @@ public class SoonRedisDao {
 	public SoonJedis getSoonJedis() {
 		return this.soonJedis;
 	}
-	
-	
+
 	public void setSoonJedis(SoonJedis soonJedis) {
 		this.soonJedis = soonJedis;
 	}
-
-
 
 	public String set(String key, String value) {
 
@@ -23,6 +23,41 @@ public class SoonRedisDao {
 			@Override
 			public String use(Jedis jedis) {
 				return jedis.set(key, value);
+			}
+		});
+
+	}
+	public String mset(Map<String,String> keyAndValues) {
+		
+		if(keyAndValues==null || keyAndValues.size() == 0){
+			return null;
+		}
+		
+		return this.soonJedis.useJedis(new SoonJedisUse<String>() {
+			
+			@Override
+			public String use(Jedis jedis) {
+				String[] keyValues = new String[keyAndValues.size() * 2];
+				int i = 0 ;
+				for(Entry<String, String> en : keyAndValues.entrySet()){
+					keyValues[i] = en.getKey();
+					keyValues[i+1] = en.getValue();
+					i = i+2;
+				}
+				
+				return jedis.mset(keyValues);
+			}
+		});
+		
+	}
+
+	public Long append(String key, String value) {
+
+		return this.soonJedis.useJedis(new SoonJedisUse<Long>() {
+
+			@Override
+			public Long use(Jedis jedis) {
+				return jedis.append(key, value);
 			}
 		});
 
@@ -39,6 +74,29 @@ public class SoonRedisDao {
 			}
 		});
 	}
-	
-	
+
+	public Long del(String key) {
+
+		return this.soonJedis.useJedis(new SoonJedisUse<Long>() {
+
+			@Override
+			public Long use(Jedis jedis) {
+
+				return jedis.del(key);
+			}
+		});
+	}
+
+	public Long incr(String key) {
+
+		return this.soonJedis.useJedis(new SoonJedisUse<Long>() {
+
+			@Override
+			public Long use(Jedis jedis) {
+
+				return jedis.incr(key);
+			}
+		});
+	}
+
 }
--- soonredis/soonredis/src/test/java/com/sooncode/soonredis/service/SoonRedisDaoTest.java
+++ soonredis/soonredis/src/test/java/com/sooncode/soonredis/service/SoonRedisDaoTest.java
@@ -1,5 +1,8 @@
 package com.sooncode.soonredis.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,9 +23,35 @@ public class SoonRedisDaoTest {
 		System.out.println("-----------------------:"+key);
 	}
 	@Test
+	public void append (){
+		Long n = soonRedisDao.append("A", " hello  redis");
+		System.out.println("-----------------------:"+n);
+	}
+	@Test
 	public void get (){
-		String value = soonRedisDao.get("A");
+		String value = soonRedisDao.get("id");
 		System.out.println("-----------------------:"+value);
+	}
+	@Test
+	public void del (){
+		Long n  = soonRedisDao.del("A");
+		System.out.println("-----------------------:"+n);
+	}
+	@Test
+	public void incr (){
+		Long n  = soonRedisDao.incr ("id");
+		System.out.println("-----------------------:"+n);
+	}
+	
+	
+	@Test
+	public void mset (){
+		Map<String,String> map = new HashMap<>();
+		map.put("name", "hechen");
+		map.put("id", "1");
+		map.put("age", "20");
+		String n  = soonRedisDao.mset(map);
+		System.out.println("-----------------------:"+n);
 	}
 	 
 }
